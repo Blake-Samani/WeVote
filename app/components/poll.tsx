647f15e44afa864c0bdef01774df45f47af050fc
Blake-Samani <blake1.samani@gmail.com>
2025-04-27 00:46:57 -0500

    Working poll meter

--- app/components/poll.tsx
+++ app/components/poll.tsx
@@ -1,5 +1,6 @@
 import React, {Component, useState} from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, Dimensions, Button} from "react-native";
+import { PieChart } from "react-native-gifted-charts";
 
 export default function Poll() {
  
@@ -10,7 +11,64 @@ export default function Poll() {
     yesPercent: 0,
     noPercent: 0,
     total: 0,
+    winner: '',
  });
+
+ const pieData = [
+   {
+     value: poll.noCount,
+     color: '#93FCF8',
+   },
+   {
+      value: poll.yesCount, 
+      color:'#009FFF', 
+      },
+ ];
+
+ const renderDot = (color) => {
+   return (
+     <View
+       style={{
+         height: 10,
+         width: 10,
+         borderRadius: 5,
+         backgroundColor: color,
+         marginRight: 10,
+       }}
+     />
+   );
+ };
+
+ const renderLegendComponent = () => {
+   return (
+     <>
+       <View
+         style={{
+           flexDirection: 'row',
+           justifyContent: 'center',
+           marginBottom: 10,
+         }}>
+         <View
+           style={{
+             flexDirection: 'row',
+             alignItems: 'center',
+             width: 120,
+             marginRight: 20,
+           }}>
+           {renderDot('#006DFF')}
+           <Text style={{color: 'white'}}>Yes</Text>
+         </View>
+         <View
+           style={{flexDirection: 'row', alignItems: 'center', width: 120}}>
+           {renderDot('#3BE9DE')}
+           <Text style={{color: 'white'}}>No</Text>
+         </View>
+       </View>
+
+     </>
+   );
+ };
+ 
 
  function incrementYes () {
    setPoll( prevPoll => (
@@ -20,9 +78,17 @@ export default function Poll() {
          total: prevPoll.total + 1,
          yesPercent: Math.round((prevPoll.yesCount/prevPoll.total) * 100), 
          noPercent: Math.round((prevPoll.noCount/prevPoll.total) * 100),
+         winner: prevPoll.yesPercent > prevPoll.noPercent ? 'Yes' : 'No',
       }
    ))
  }
+
+ /**todo:
+   create logic that gives 50/50
+   currently 50/50 defaults to one side having 1 percentage more
+   possible fix is math.round at time of render vs in function
+   repeated code in increment functions, figure out how to consolidate
+    */
  function incrementNo () {
    setPoll( prevPoll => (
       {
@@ -31,6 +97,7 @@ export default function Poll() {
          total: prevPoll.total + 1,
          yesPercent: Math.round((prevPoll.yesCount/prevPoll.total) * 100), 
          noPercent: Math.round((prevPoll.noCount/prevPoll.total) * 100),
+         winner: prevPoll.yesPercent > prevPoll.noPercent ? 'Yes' : 'No',
       }
    ))
  }
@@ -45,6 +112,42 @@ export default function Poll() {
          <Text>Percent Yes: {poll.yesPercent} %</Text>
          <Text>Percent No: {poll.noPercent} %</Text>
          <Text>Total: {poll.total}</Text>
+         <View
+      style={{
+        margin: 20,
+        padding: 16,
+        borderRadius: 20,
+        backgroundColor: '#232B5D',
+      }}>
+      <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>
+         Results
+      </Text>
+      <View style={{padding: 20, alignItems: 'center'}}>
+        <PieChart
+          data={pieData}
+          donut
+          showGradient
+          sectionAutoFocus
+          radius={90}
+          innerRadius={60}
+          innerCircleColor={'#232B5D'}
+          centerLabelComponent={() => {
+            return (
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Text
+                  style={{fontSize: 22, color: 'white', fontWeight: 'bold'}}>
+                  {poll.winner}
+                </Text>
+                <Text style={{fontSize: 14, color: 'white'}}>
+                  {poll.winner == 'Yes' ? poll.yesPercent : poll.noPercent} %
+                  </Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+      {renderLegendComponent()}
+    </View>
     </View>
  )
 }
--- app/tabs/settings.tsx
+++ app/tabs/settings.tsx
@@ -2,16 +2,19 @@ import { Text, View, StyleSheet, TouchableOpacity, Dimensions, Button} from "rea
 import  Poll from "../components/poll"
 import { Link, Tabs } from 'expo-router';
 
-const Settings = () => {
-    
 
-    return ( <View>
+
+const Settings = () => {
+
+
+    return ( <View style={{paddingVertical: 100, flex: 1}}>
         <Link href="/" asChild>
             <Button 
             title="settings pages/ back to home"
             ></Button>
         </Link>
         <Poll></Poll>
+        
         
     </View> );
 }
