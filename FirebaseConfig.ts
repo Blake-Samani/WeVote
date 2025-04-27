5dbbb2f1648428a8a34d684515607dcfc5adb921
Blake-Samani <blake1.samani@gmail.com>
2025-04-27 16:51:38 -0500

    config changes

---
+++ FirebaseConfig.ts
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+ import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+// import {...} from 'firebase/database';
+// import {...} from 'firebase/firestore';
+// import {...} from 'firebase/functions';
+ import {getStorage} from 'firebase/storage';
+ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyACxLjmsA4iM0MuPALaKHkKeQTq3dIxsvs',
+  authDomain: 'wvote-ce6f4.firebaseapp.com',
+  databaseURL: 'https://wvote-ce6f4.firebaseio.com',
+  projectId: 'wvote-ce6f4',
+  storageBucket: 'wvote-ce6f4.firebasestorage.app',
+  messagingSenderId: '169615202545',
+  appId: '1:169615202545:web:e7a299deb3bec74192b150',
+  measurementId: 'G-1ZRCP33FHJ',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app,{
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
